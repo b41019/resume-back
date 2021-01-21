c6a19f6eec0e3f192ef69ed45d7ee80cecfc8d41
b41019 <b41019@gmail.com>
2021-01-21 16:29:16 +0800

    前台範本

--- db88.sql
+++ db88.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2020-12-07 04:16:22
--- 伺服器版本： 10.4.17-MariaDB
--- PHP 版本： 7.4.13
+-- 產生時間： 2021-01-21 09:07:39
+-- 伺服器版本： 10.4.14-MariaDB
+-- PHP 版本： 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `db99`
+-- 資料庫： `db88`
 --
 
 -- --------------------------------------------------------
@@ -34,6 +34,14 @@ CREATE TABLE `ad` (
   `sh` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 傾印資料表的資料 `ad`
+--
+
+INSERT INTO `ad` (`id`, `text`, `img`, `sh`) VALUES
+(1, '管理登入', '', 1),
+(2, '516322', '', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -46,6 +54,34 @@ CREATE TABLE `admin` (
   `pw` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 傾印資料表的資料 `admin`
+--
+
+INSERT INTO `admin` (`id`, `acc`, `pw`) VALUES
+(1, 'admin', '123456'),
+(2, 'mem01', '0000');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `auto`
+--
+
+CREATE TABLE `auto` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `auto`
+--
+
+INSERT INTO `auto` (`id`, `text`, `img`, `sh`) VALUES
+(1, '5566不能亡\r\n', '', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -56,6 +92,53 @@ CREATE TABLE `bottom` (
   `id` int(11) UNSIGNED NOT NULL,
   `bottom` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `bottom`
+--
+
+INSERT INTO `bottom` (`id`, `bottom`) VALUES
+(1, '123');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `edu`
+--
+
+CREATE TABLE `edu` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `edu`
+--
+
+INSERT INTO `edu` (`id`, `text`, `img`, `sh`) VALUES
+(1, '學店大學肥宅系\r\n', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `hshot`
+--
+
+CREATE TABLE `hshot` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `hshot`
+--
+
+INSERT INTO `hshot` (`id`, `text`, `img`, `sh`) VALUES
+(2, '', 'Brandon Ingram.jpg', 0);
 
 -- --------------------------------------------------------
 
@@ -69,6 +152,27 @@ CREATE TABLE `image` (
   `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `sh` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `job`
+--
+
+CREATE TABLE `job` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `job` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pay` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lo` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `job`
+--
+
+INSERT INTO `job` (`id`, `job`, `pay`, `lo`, `sh`) VALUES
+(2, '正值', '200000', '家裡', 0);
 
 -- --------------------------------------------------------
 
@@ -110,6 +214,62 @@ CREATE TABLE `news` (
   `sh` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 傾印資料表的資料 `news`
+--
+
+INSERT INTO `news` (`id`, `text`, `img`, `sh`) VALUES
+(1, '1233321', '', 1),
+(2, '5566556', '', 1),
+(3, '789789', '', 1),
+(4, '4654898', '', 1),
+(5, '22222222', '', 1),
+(6, '88888888888888', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `prof`
+--
+
+CREATE TABLE `prof` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sh` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `prof`
+--
+
+INSERT INTO `prof` (`id`, `text`, `img`, `sh`) VALUES
+(1, 'css夜空', '未命名.png', 0),
+(2, 'css新聞', '未命名1.png', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `self`
+--
+
+CREATE TABLE `self` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tel` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birth` date NOT NULL,
+  `sh` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `self`
+--
+
+INSERT INTO `self` (`id`, `name`, `tel`, `email`, `birth`, `sh`) VALUES
+(1, '111', '322', 'fatfatguy@gmail.com', '2020-12-31', 0),
+(2, '肥宅', '03248787', 'fatfatguy@gmail.com', '2020-12-29', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -123,6 +283,14 @@ CREATE TABLE `title` (
   `sh` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 傾印資料表的資料 `title`
+--
+
+INSERT INTO `title` (`id`, `text`, `img`, `sh`) VALUES
+(4, '1323', '01B02.jpg', 0),
+(5, '331355', '01D10.jpg', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -133,6 +301,13 @@ CREATE TABLE `total` (
   `id` int(11) UNSIGNED NOT NULL,
   `total` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `total`
+--
+
+INSERT INTO `total` (`id`, `total`) VALUES
+(1, 33);
 
 --
 -- 已傾印資料表的索引
@@ -151,15 +326,39 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `auto`
+--
+ALTER TABLE `auto`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `bottom`
 --
 ALTER TABLE `bottom`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `edu`
+--
+ALTER TABLE `edu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `hshot`
+--
+ALTER TABLE `hshot`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `image`
 --
 ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `job`
+--
+ALTER TABLE `job`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -178,6 +377,18 @@ ALTER TABLE `mvim`
 -- 資料表索引 `news`
 --
 ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `prof`
+--
+ALTER TABLE `prof`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `self`
+--
+ALTER TABLE `self`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -200,25 +411,49 @@ ALTER TABLE `total`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `ad`
 --
 ALTER TABLE `ad`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `auto`
+--
+ALTER TABLE `auto`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `bottom`
 --
 ALTER TABLE `bottom`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `edu`
+--
+ALTER TABLE `edu`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `hshot`
+--
+ALTER TABLE `hshot`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `image`
 --
 ALTER TABLE `image`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `job`
+--
+ALTER TABLE `job`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `menu`
@@ -236,19 +471,31 @@ ALTER TABLE `mvim`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `prof`
+--
+ALTER TABLE `prof`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `self`
+--
+ALTER TABLE `self`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `title`
 --
 ALTER TABLE `title`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `total`
 --
 ALTER TABLE `total`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
