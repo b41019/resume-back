8f0732dd1af4b52c359bbe918e4982d3c81669bc
b41019 <b41019@gmail.com>
2021-01-22 08:39:57 +0800

    試推

--- front/main.php
+++ front/main.php
@@ -6,32 +6,32 @@
     </ol>
     <div class="carousel-inner">
       <div class="carousel-item vh-100 active">
-        <img src="imgs/carousel_1.jpg" class="h-100 w-100">
+        <img src="imgs/howg.jpg" class="h-100 w-100">
         <div class="carousel-caption d-flex flex-column justify-content-center">
-          <h1>泰山渡假飯店 您的唯一首選</h1>
-          <p class="d-none d-md-block">忙碌的生活中，你有多久沒有好好的考賞自己呢？放慢腳步，來泰山好好度假吧</p>
+          <h1>霍格華茲酒</h1>
+          <p class="d-none d-md-block">忙碌的生活中，你有多久沒有好好的考賞自己呢？來一杯威士忌吧 <small>廣告贊助</small></p>
         </div>
       </div>
       <div class="carousel-item vh-100">
-        <img src="imgs/carousel_2.jpg" class="h-100 w-100">
+        <img src="imgs/banner.jpg" class="h-100 w-100">
         <div class="carousel-caption d-flex flex-column justify-content-center">
-          <h1>泰山咖啡廳 世界聞名而來</h1>
-          <p class="d-none d-md-block">泰山特產咖啡精選淬鍊而成，凡住宿者每人免費請你喝經典咖啡下午茶一份</p>
+          <h1>泰山職訓局,輔導你就業</h1>
+          <p class="d-none d-md-block">泰山免費受訓,即日氣接受報名  <small>廣告贊助</small></p>
         </div>
       </div>
       <div class="carousel-item vh-100">
-        <img src="imgs/carousel_3.jpg" class="h-100 w-100">
+        <img src="imgs/cal.jpg" class="h-100 w-100">
         <div class="carousel-caption d-flex flex-column justify-content-center">
-          <h1>寵愛有你 婚宴專案特惠</h1>
-          <p class="d-none d-md-block">本月活動喜宴專案滿10桌送一主桌，滿25桌再送沖繩雙人蜜月來回機票</p>
+          <h1>牛排餐廳牛年月曆,新上市</h1>
+          <p class="d-none d-md-block">本月活動壽星專案滿1000送月曆,再抽沖繩雙人蜜月來回機票  <small>廣告贊助</small></p>
         </div>
       </div>
-      <div class="carousel-item vh-100">
+      <!-- <div class="carousel-item vh-100">
         <img src="imgs/carousel_4.jpg" class="h-100 w-100">
         <div class="carousel-caption d-flex flex-column justify-content-center">
           <h1>外燴商餐 美食饗宴</h1>
           <p class="d-none d-md-block">全台最豐富的國內外文化餐飲美食，與先進的視聽投影設備與企劃小組，歡迎商約洽談</p>
-        </div>
+        </div> -->
       </div>
     </div>
     <a class="carousel-control-prev" href="#lokislider" data-slide="prev">
